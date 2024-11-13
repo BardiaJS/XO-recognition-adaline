f99e929c74dd9a410ecc4ce7290d19ea50530c58
BardiaJhS <bsardroodi@gmail.com>
2024-11-14 00:14:18 +0330

    wrote the project and debug

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.final_xo_adaline {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.final_xo_adaline to javafx.fxml;
+    exports com.example.final_xo_adaline;
+}
